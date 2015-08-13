0e77506e4e1f31d85b4ff2f283bbf3822344df94
manuelleduc <manuel.leduc@gmail.com>
2015-08-13 03:22:34 -0700

    First annotations definition

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Org.Kevoree.Annotation
+{
+    class ChannelType: Attribute { }
+    class ComponentType : Attribute { }
+    class GroupType : Attribute { }
+    class Input : Attribute { }
+    class KevoreeInject : Attribute { }
+    class NodeType : Attribute { }
+    class Output : Attribute { }
+    class Param: Attribute { }
+    class Start : Attribute { }
+    class Stop : Attribute { }
+    class Update : Attribute { }
+}
